f1b3abba98a319057844ab7f6d9fc537787fb4f8
AlanSalgado <s20120202@alumnos.itsur.edu.mx>
2024-06-18 21:28:53 -0600

    Pruebas para verificar build's

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PURO PINCHE <code>RUMAPP</code> ALV 🤑👌
         </p>
         <a
           className="App-link"
